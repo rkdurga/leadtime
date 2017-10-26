9fad63a4e0b162a9df185b4646dda3d88117f96e
Ramesha <axr6441@d25rt1ekgq17im.amer.homedepot.com>
2017-10-26 17:28:10 -0400

    leadtime initial version

---
+++ src/main/java/com/homedepot/supplychain/ipp/importleadtimetest/ImportLeadTimeTestApplication.java
@@ -0,0 +1,13 @@
+package com.homedepot.supplychain.ipp.importleadtimetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportLeadTimeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportLeadTimeTestApplication.class, args);
+		System.out.println("Hello");
+	}
+}
---
+++ src/test/java/com/homedepot/supplychain/ipp/importleadtimetest/ImportLeadTimeTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.homedepot.supplychain.ipp.importleadtimetest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImportLeadTimeTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
